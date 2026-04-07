2f507f4a8a81db1aba2af8003c569c354ccfefee
Heitor-OC <heitorocosta@gmail.com>
2026-04-07 17:27:04 -0300

    creating spring project

---
+++ backend/running-tracker/src/main/java/com/heitor/running/running_tracker/RunningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.heitor.running.running_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/running-tracker/src/test/java/com/heitor/running/running_tracker/RunningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitor.running.running_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
